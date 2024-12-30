d27c085cf91cbf1a3e9b5afab5306e2641eec576
Murat Kılıç <158284118+muratkilic04@users.noreply.github.com>
2024-12-30 13:18:50 +0300

    Add files via upload

---
+++ BirSesliHarfKalın mı İnce mi/sesliHarf/src/sesliHarf/kalinmiIncemi.java
@@ -0,0 +1,29 @@
+package sesliHarf;
+
+
+
+public class kalinmiIncemi {
+
+	public static void main(String[] args) {
+		
+		
+		char harf='e';
+		
+		
+		switch (harf) {
+	     	case 'A':
+		    case 'I':
+		    case 'O':
+		    case 'U':
+		    	System.out.println("Kalın sesli harf");
+		    	break;
+		    	default:
+		    		System.out.println("İnce sesli harf");
+		
+		
+		               
+		}
+		
+	}
+
+}
